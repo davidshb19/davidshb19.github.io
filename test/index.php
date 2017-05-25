a31f6fe96c84460336a90919a2e7b8a1cf9ed99a
harry davidson <myps3ylod@gmail.com>
2017-05-25 10:56:32 -0400

    ilite stuff

---
+++ test/index.php
@@ -0,0 +1,432 @@
+<!doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-gb" class="no-js"> <!--<![endif]-->
+<head>
+	<title>ILITE Cyber | Home</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+    
+	<link rel="shortcut icon" href="images/favicon.ico">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Dancing+Script:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://brick.a.ssl.fastly.net/Roboto:400"/>
+    <!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/simpleline-icons/simple-line-icons.css" media="screen" />
+	<link href="js/animations/css/animations.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="stylesheet" media="screen" href="css/responsive-layouts.css" type="text/css" />
+	<link rel="stylesheet" href="js/flexslider/flexslider.css" type="text/css" media="screen" />
+ 	<link rel="stylesheet" type="text/css" href="js/flexslider/skin.css" />
+</head>
+
+<body>
+
+<div class="site_wrapper home">
+
+<div id="wrap" class="home__top-style right-0">
+
+<!--Header-->
+<link rel="import" href="header.php">
+
+<div class="home__wrap" >
+
+	<div id="home__banner" class="home__banner">
+
+		<div class="home__banner__text-container">
+			<h1>We are ilite cyber.</h1>
+			<h2>Cyberpatriot Team //team number\\</h2>
+			<h3>//get someone to replace\\ We are a team founded on the principles of leadership, teamwork, and academic rigor. Through competitions, outreach, and mentorship we passionately advocate for science and technology in our schools, communities, and throughout the world.</h3>
+		</div>
+<!--
+    <div class="less5 centf bigtfont gray">Here at ILITE First Team 1885, we believe that every student is a leader. Through our different subteams of business, graphics, electronics, programming, and build, we work very hard to give every team member the opportunity to learn and lead. We have created an environment in which FIRST Robotics is so loved that our alumni volunteer across the nation even after graduating. </div><br>
+
+	<div class="less5 centf bigtfont gray">Our leadership reaches beyond of FIRST Team 1885. We have created an identity for ourselves in our community, county, state, and nation. ILITE reaches out to our community through town festivals, charitable involvement, elementary school tutoring, and hosting our annual Haymaker STEM Expo. On a broader scale, ILITE members have lobbied politicians through their School Board and meetings at the Capitol to encourage the advancement of STEM in education. </div><br>
+
+	<div class="less5 centf bigtfont gray">Whether it be through our summer camps that teach hundreds of students about STEM, or our annually-hosted FIRST competitions, ILITE continuously inspires those around them. We strive to embody the FIRST mission of "More than Robots" in all we do."</div>
+ -->
+
+<div class="center-button">
+<a href="/about/missionandvision.php" class="button thhthree">LEARN MORE ABOUT OUR MISSION AND VISION!</a> </div>
+</div><!-- end section -->
+
+<div id="wrap5">
+
+	<div class="featured_section142 A">
+		<div class="container">
+
+			<p class="home__section-header">events</p>
+			<p class="home__section-sub-header">Come support Team //team number\\ and our work in the community.</p>
+
+  		<div class="clearfix margin_top3"></div>
+
+		  <div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="250">
+
+				<div class="home__img-holder">
+					<div class="home__img e1"></div>
+				</div>
+
+		  <div class="addes">
+		      <em><a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007"><i class="fa fa-map-marker"></i> Google Map</a></em>
+		      <strong><a href="https://www.google.com/">//summer camps\\</a></strong>
+					<strong>//Date Range\\</strong>
+					15000 Graduation Dr, Haymarket, VA 20169
+				</div>
+
+			</div><!-- end section -->
+			
+			<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="200">
+
+				<div class="home__img-holder">
+<div class="home__img e2"></div>
+				
+				</div>
+ <div class="addes">
+		      <em><a href="//google maps location\\"><i class="fa fa-map-marker"></i> Google Map</a></em>
+		      <strong><a href="community/hosted.php#ftc">//event name\\</a></strong>
+					<strong>//date\\</strong>
+					//location\\
+				</div>
+
+			</div><!-- end section -->
+
+		  <div class="one_third last animate" data-anim-type="fadeInUp" data-anim-delay="300">
+
+				<div class="home__img-holder">
+					<div class="home__img e3"></div>
+				</div>
+
+		    <div class="addes">
+		      <em><a href="//google maps location\\"><i class="fa fa-map-marker"></i> Google Map</a></em>
+		 	      <strong><a href="community/hosted.php#frc">//event name\\</a></strong>
+			<strong>//date\\</strong>
+					//location\\
+				</div>
+
+			</div><!-- end section -->
+		</div>
+	</div> <!-- end featured section 142 -->
+
+	<div class="B home__news-container">
+		<div class="container">
+
+			<p class="home__section-header white">news</p>
+			<p class="home__section-sub-header white">
+				Stay up to date with the competitions, exhibitions, and educational outreach
+				<span class="desktop-break">that ILITE frequently hosts and participates in.</span>
+			</p>
+
+			<div class="clearfix margin_top3"></div>
+
+			<div class="flexbox-columns">
+
+				<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="200">
+					<a class="home__img-holder" href="#">
+						<div class="home__img n3"></div> 
+					</a>
+					<div class="addes">
+						<center><strong><a href="http://www.signupgenius.com/go/30e084faea929a0fc1-ilite3">ILITE End of Year celebration</a></strong></center>
+						<span class="desktop-break">ILITE Robotics and ILITE Cyber will be celebrating their successful year in this end of the year celebration. Sign up to bring food or sporting equipment to be used in the many fields/courts available at Long Park.</div>
+					
+				
+				</div><!-- end section -->
+
+				<div class="one_third animate" data-anim-type="fadeInUp" data-anim-delay="250">
+					<a class="home__img-holder" href="#">
+						<div class="home__img n1"></div> 
+					</a>
+						<div class="addes">
+						<strong><a href="http://google.com/">//news article\\</a></strong>
+						<span class="desktop-break">//bref\\With 33 registered teams, the 2016 ILITE Robotics Off-Season Challenge was the largest ever held. The winning alliance was composed of FRC Teams 384, Sparky Robotics, 5338, RoboLoCos, and 2363, Triple Helix. Congratulations teams!</div>
+					
+				
+				</div><!-- end section -->
+
+				<div class="one_third last animate" data-anim-type="fadeInUp" data-anim-delay="300">
+
+					<a class="home__img-holder" href="#">
+						<div class="home__img n2"></div> 
+					</a>
+<div class="addes">
+						<strong><a href="http://google.com/">//news article\\</a></strong>
+						<span class="desktop-break">//bref\\ILITE has been chosen to be kindness ambassadors for our school and community. Recent developments include enacting kindness rules at our high school, starting a kindness week at out school, and spreading kindess at the PWCS Walk for Autism.</div>
+					
+					
+					
+				</div><!-- end section -->
+			</div>
+
+		</div>
+	</div> <!-- end featured section 142 -->
+
+	<div class="clearfix"></div>
+
+
+	<div class="home__sponsors">
+
+		<div class="home__sponsors__container">
+
+			<p class="home__section-header">our sponsors</p>
+			<p class="home__section-sub-header">We thank our sponsors for their continued support.</p>
+
+			<div class="home__sponsors__logo-wrap">
+
+				<a href="http://www.netapp.com/us/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/1.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.a1floodtech.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/2.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.lockheedmartin.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/3.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://battlefieldhs.schools.pwcs.edu/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/4.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.usstem.org/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/17.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="https://www.macedontechnologies.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/6.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.frdallc.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/7.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.bellpumpandwell.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/8.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="https://www.incadencecorp.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/9.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="#" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/11.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="https://www.novec.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/12.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.afcea.org/site/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/13.gif )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href=" http://ascoweb.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/14.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.pwcs.edu/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/15.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://wanderstockphoto.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/16.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="https://www.army.mil/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/6.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.datasynctech.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/5.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="https://www.geico.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/18.png )" class="home__sponsors__logo"></div>
+				</a>
+
+				<a href="http://www.azosai.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/19.jpg )" class="home__sponsors__logo"></div>
+				</a>
+
+<a href="http://khouriortho.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/20.jpg )" class="home__sponsors__logo"></div>
+				</a>
+				
+				<a href="www.townofhaymarket.org/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/21.jpg )" class="home__sponsors__logo"></div>
+				</a>
+				
+				<a href="https://www.tropicalsmoothiecafe.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/22.png )" class="home__sponsors__logo"></div>
+				</a>
+				<a href="http://www.cathyshometeam.com/" class="home__sponsors__logo-link" target="_blank">
+					<div style="background-image: url( images/sponsors/23.png )" class="home__sponsors__logo"></div>
+				</a>
+				
+			</div>
+		</div>
+
+	</div>
+
+
+
+</div><!-- end events section -->
+
+<div class="clearfix"></div>
+
+
+<div class="footer">
+  <div class="footerInfo">
+
+    <div class="col" >
+      <h5 class="footer__header">CONTACT US</h5>
+      <a class="footer__link" href="mailto:gddrake@gmail.com" >drakegd@pwcs.edu</a>
+      <p class="footer__text">Coach Gail Drake</p>
+      <p class="footer__text">(571)261-4726</p>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">LOCATION</h5>
+      <a class="footer__link" href="/about/contactus.php" >
+        <i class="fa fa-map-marker"></i>
+        Map &amp; Directions
+      </a>
+      <p class="footer__text">ILITE Cyber</p>
+      <p class="footer__text">15000 Graduation Drive</p>
+      <p class="footer__text">Haymarket, VA 20169</p>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">CALENDAR</h5>
+      <a class="footer__link" href="/getinvolved/whereisilite.php">
+        <i class="fa fa-calendar-o"></i>
+        Team Events &amp; Schedule
+      </a>
+    </div>
+
+    <div class="col" >
+      <h5 class="footer__header">GET CONNECTED</h5>
+      <a href="https://www.facebook.com/iliterobotics/" target="_blank" class="footer__social">
+        <i class="fa fa-facebook"></i>
+      </a>
+      <a href="https://www.twitter.com/team1885" target="_blank" class="footer__social">
+        <i class="fa fa-twitter"></i>
+      </a>
+      <a href="https://www.youtube.com/channel/UCCWs22DHGwRloB613zOJ9UA" target="_blank" class="footer__social">
+        <i class="fa fa-youtube"></i>
+      </a>
+      <a href="https://www.instagram.com/team1885" target="_blank" class="footer__social">
+        <i class="fa fa-instagram"></i>
+      </a>
+    </div>
+  </div>
+
+
+</div>
+
+<div class="footer__copyright">
+  <p>&copy; <span id="copyright-year"></span> ILITE Cyber. All rights reserved.</p>
+</div>
+
+
+<a href="#" class="scrollup">Scroll</a><!-- end scroll to top of the page-->
+
+</div>
+
+<nav id='side-menu' class='menu width-0'>
+	<div class="nav-container">
+
+    <div id="side-menu__about" class="nav__link">
+			About <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__about__wrapper">
+				<a href='/about/missionandvision.php' class="nav__link__target">Mission &amp; Vision</a>
+        <a href='/about/team.php' class="nav__link__target">Team</a>
+				<a href='/about/history.php' class="nav__link__target">History</a>
+				<a href='/about/press.php' class="nav__link__target">Press</a>
+				<a href='/about/contactus.php' class="nav__link__target">Contact Us</a>
+			</div>
+		</div>
+
+    <div id="side-menu__first" class="nav__link">
+			Cyberpatriot <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__first__wrapper">
+				<a href='/first/whatisfirst.php' class="nav__link__target">About Cyberpatriot</a>
+				<a href='/first/scholarships.php' class="nav__link__target">Scholarships</a>
+				<a href='/first/firstcampaign.php' class="nav__link__target">FIRST Outreach</a>
+        <a href='/first/season.php' class="nav__link__target">FIRST 2016 Season</a>
+			</div>
+		</div>
+
+    <div id="side-menu__community" class="nav__link">
+			Community <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__community__wrapper">
+				<a href='/community/sponsors.php' class="nav__link__target">Sponsors</a>
+				<a href='/community/summer-camps.php' class="nav__link__target">Summer Camps</a>
+								<a href='/community/surveys.php' class="nav__link__target">Summer Camps Surveys</a>
+				<a href='/community/outreach.php' class="nav__link__target">Outreach</a>
+				<a href='/community/us-stem-foundation.php' class="nav__link__target">US STEM Foundation</a>
+				<!-- <a href='/community/hosted-events.php' class="nav__link__target">Hosted Events</a> -->
+			</div>
+		</div>
+
+    <a href='/media/media.php' class="nav__link no-caret">Media</a>
+
+    <div id="side-menu__involved" class="nav__link action">
+			Get Involved <div class="caret"></div>
+			<div class="nav__link__wrapper" id="side-menu__involved__wrapper">
+				<a href='/getinvolved/whereisilite.php' class="nav__link__target">Where is Team 1885</a>
+				<a href='/getinvolved/volunteereventportal.php' class="nav__link__target">Volunteer Portal</a>
+				<a href='/getinvolved/studenteventportal.php' class="nav__link__target">Student Event Portal</a>
+				<a href='/getinvolved/becomeasponsor.php' class="nav__link__target">Become a Sponsor</a>
+				<a href='/getinvolved/becomeamember.php' class="nav__link__target">Become a Member</a>
+				<a href='/getinvolved/becomeamentor.php' class="nav__link__target">Become a Mentor</a>
+				<a href='/getinvolved/firstfuel.php' class="nav__link__target">Help Feed the Team</a>
+			</div>
+		</div>
+</nav>
+
+<!-- ######### JS FILES ######### -->
+<!-- get jQuery -->
+<script type="text/javascript" src="js/universal/jquery.js"></script>
+
+<!-- Whole site JS -->
+<script src="js/ilite/min/ilite.min.js" type="text/javascript"></script>
+
+<!-- Home page only JS -->
+<script src="js/home/min/home.min.js" type="text/javascript"></script>
+
+<script src="js/animations/js/animations.min.js" type="text/javascript"></script>
+
+<script src="js/onepage3/bootstrap.min.js"></script>
+<script src="js/onepage3/jPinning.js"></script>
+<script type="text/javascript" src="js/home/button.js"></script>
+<script src="js/scrolltotop/totop.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/mainmenu/modernizr.custom.75180.js"></script>
+
+<script type="text/javascript" src="js/cubeportfolio/jquery.cubeportfolio.min.js"></script>
+<script type="text/javascript" src="js/cubeportfolio/main41.js"></script>
+
+<!-- <script src="js/carouselowl/owl.carousel.js"></script> -->
+
+<script type="text/javascript" src="js/cform/form-validate.js"></script>
+
+<script src="js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+
+<script src="js/progressbar/progress.js" type="text/javascript" charset="utf-8"></script>
+
+<script defer src="js/flexslider/jquery.flexslider.js"></script>
+<script defer src="js/flexslider/custom.js"></script>
+
+
+</body>
+</html>
